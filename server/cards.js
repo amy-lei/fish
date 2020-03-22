8d32440fee407d1cd1433da3ad85f6ceb3eab2bb
Amy <leiamy12@gmail.com>
2020-03-22 18:12:45 -0400

    moved card obj generator to backend; deals card

---
+++ server/cards.js
@@ -0,0 +1,51 @@
+const SIZE = 54;
+const SUITS = [
+    'heart', 
+    'diamond', 
+    'spade', 
+    'club',
+  ];
+const RANKS = [
+    'ace',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'jack',
+    'queen',
+    'king',
+];
+  
+const generateCards = (n) => {
+    let cards = [];
+    // generate the 52 normal cards
+    for (let suit of SUITS) {
+        for (let rank of RANKS) {
+            cards.push({
+                rank: rank,
+                suit: suit,
+            });
+        }
+    }
+
+    // account for the two jokers
+    cards.push({ rank: "joker", suit: "black"});
+    cards.push({ rank: "joker", suit: "red"});
+
+    let output = [];
+    const numCards = SIZE / n;
+    cards.sort(() => Math.random() - 0.5);
+
+    for(let i = 0; i < n; i++) {
+        output.push(cards.slice(i * numCards, i * numCards + numCards));
+    }
+    
+    return output;
+}
+
+module.exports = generateCards;
