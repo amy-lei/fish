d4a77deecba2ffc8faf37f45a429fc04e0eccd04
Amy <leiamy12@gmail.com>
2020-05-14 11:02:52 -0400

    styled history and votes

--- client/src/components/modules/GameHistory.js
+++ client/src/components/modules/GameHistory.js
@@ -9,17 +9,17 @@ class GameHistory extends Component {
         this.state = {};
     }
 
-    render() {
-        const history = this.props.history.map(move => {
+    createHistory = (history, all) => {
+        return history.map(move => {
             if (move.type === 'ASK')
                 return (
-                    <div className={`message history_move ${this.props.all?"left":""}`}>
+                    <div className={`message history_move ${all? "left":""}`}>
                         <div className={`message_img ${
                             move.asker.index % 2 == 0? 'team-even' : 'team-odd'}`}>
                             {FACES[move.asker.index]} 
                         </div>
-                        <div className="message_info">
-                            <div className={`message_info-sender history_move-who ${this.props.all ? "more-space": ""}`}>
+                        <div className={`message_info ${!all && 'main-view'}`}>
+                            <div className={`message_info-sender history_move-who ${all ? "more-space": ""}`}>
                                 {move.asker.name} asked 
                             </div>
                             <div className="message_info-content history_move-what">
@@ -32,13 +32,13 @@ class GameHistory extends Component {
                 const result = move.success ? "did" : "did not";
                 return (
                     <>
-                        <div className={`message history_move ${this.props.all?"left":""}`}>
+                        <div className={`message history_move ${all?"left":""}`}>
                             <div className={`message_img ${
                                 move.responder.index % 2 == 0? 'team-even' : 'team-odd'}`}>
                                 {FACES[move.responder.index]} 
                             </div>
-                            <div className="message_info">
-                                <div className={`message_info-sender history_move-who ${this.props.all ? "more-space": ""}`}>
+                            <div className={`message_info ${!all && 'main-view'}`}>
+                                <div className={`message_info-sender history_move-who ${all ? "more-space": ""}`}>
                                     {move.responder.name} said
                                 </div>
                                 <div className="message_info-content history_move-what">
@@ -53,11 +53,29 @@ class GameHistory extends Component {
                 );
             }
         });
+    }
+
+    createTurnInfo = (whoseTurn, turnType) => {
+        return <label className='turn-type'>
+            {`It is ${whoseTurn}'s turn to ${turnType}`}
+        </label>
+    }
+
+    render() {
+        const {
+            history,
+            all,
+            whoseTurn,
+            turnType,
+        } = this.props;
+        const historyInfo = this.createHistory(history, all);
+
         return (
-            <div className={`messages history ${this.props.hidden ? "hidden" : ""}`} hidden={this.props.hidden}>
-                {this.props.all 
-                    ? history
-                    : history[history.length - 1]}
+            <div className={`messages history ${!all && 'history-main-view'} ${this.props.hidden ? "hidden" : ""}`}>
+                {!all && this.createTurnInfo(whoseTurn, turnType)}
+                {all 
+                    ? historyInfo
+                    : historyInfo[historyInfo.length - 1]}
             </div>
         );
     }
@@ -65,6 +83,8 @@ class GameHistory extends Component {
 
 const mapStatesToProps = (state) => ({
     history: state.history,
+    whoseTurn: state.turnInfo.whoseTurn,
+    turnType: state.turnInfo.turnType,
 });
 
 export default connect(mapStatesToProps)(GameHistory);
